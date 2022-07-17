05f98ee10b90d377b9c1f91f8ca4086d11be8eb4
Naton1 <28943608+Naton1@users.noreply.github.com>
2022-07-16 20:27:19 -0700

    Work around issue when attaching to older jvms

--- explorer/src/main/java/com/github/naton1/jvmexplorer/agent/RunningJvm.java
+++ explorer/src/main/java/com/github/naton1/jvmexplorer/agent/RunningJvm.java
@@ -42,11 +42,23 @@ public class RunningJvm {
 			log.debug("Attempting to load agent: {}", this);
 			final String localPath = agentPreparer.loadAgentOnFileSystem(PATH);
 			final VirtualMachine vm = VirtualMachine.attach(id);
-			vm.loadAgent(localPath, id + ":" + name);
-			vm.detach();
-			log.debug("Loaded agent: {}", this);
+			try {
+				vm.loadAgent(localPath, id + ":" + name);
+				vm.detach();
+				log.debug("Loaded agent: {}", this);
+			}
+			finally {
+				vm.detach();
+			}
 		}
 		catch (IOException | UncheckedIOException | AttachNotSupportedException | AgentLoadException | AgentInitializationException e) {
+			if (e instanceof AgentLoadException && "0".equals(e.getMessage())) {
+				log.debug("Received AgentLoadException while attaching but message is '0'.", e);
+				// See https://stackoverflow.com/questions/54340438
+				// The implementation of attaching changed so when attaching to older JVMs (like java 8).
+				// Older JVMs looked for '0' but newer JVMs are looking for 'return code: 0' when attaching.
+				return;
+			}
 			log.warn("Failed to attach to jvm: " + this, e);
 			throw new AgentException(e.getMessage(), e);
 		}
