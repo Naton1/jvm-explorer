1e8c7ccee53f03af8d7cffc230c07681751214ec
Naton1 <28943608+Naton1@users.noreply.github.com>
2022-07-24 09:23:53 -0700

    Fix possible race condition in tests

--- explorer/src/test/java/com/github/naton1/jvmexplorer/net/PacketResponseHandlerTest.java
+++ explorer/src/test/java/com/github/naton1/jvmexplorer/net/PacketResponseHandlerTest.java
@@ -14,21 +14,25 @@ import java.util.stream.IntStream;
 class PacketResponseHandlerTest {
 
 	@Test
-	void givenValidPackets_whenStreamPacketsAndEndReceivedAfterAllPackets_thenStreamProcessedSuccessfully() {
+	void givenValidPackets_whenStreamPacketsAndEndReceivedAfterAllPackets_thenStreamProcessedSuccessfully()
+			throws InterruptedException {
 		final AtomicBoolean cleanupCalled = new AtomicBoolean(false);
 		final AtomicInteger updateCount = new AtomicInteger(0);
 		final PacketResponseHandler<String> packetResponseHandler =
 				new PacketResponseHandler<>(() -> cleanupCalled.set(
 				true), updateCount::set);
 
-		new Thread(() -> {
+		final Thread packetProcessor = new Thread(() -> {
 			packetResponseHandler.onPacketReceived(createPackets(1000));
 			packetResponseHandler.onPacketReceived(createPackets(1000));
 			packetResponseHandler.receivedEnd(2000);
-		}).start();
+		});
+		packetProcessor.start();
 
 		final List<String> results = packetResponseHandler.getPacketStream(30, TimeUnit.SECONDS)
 		                                                  .collect(Collectors.toList());
+
+		packetProcessor.join();
 
 		Assertions.assertTrue(cleanupCalled.get());
 		Assertions.assertEquals(2000, updateCount.get());
@@ -36,21 +40,25 @@ class PacketResponseHandlerTest {
 	}
 
 	@Test
-	void givenValidPackets_whenStreamPacketsAndEndReceivedBeforeAllPackets_thenStreamProcessedSuccessfully() {
+	void givenValidPackets_whenStreamPacketsAndEndReceivedBeforeAllPackets_thenStreamProcessedSuccessfully()
+			throws InterruptedException {
 		final AtomicBoolean cleanupCalled = new AtomicBoolean(false);
 		final AtomicInteger updateCount = new AtomicInteger(0);
 		final PacketResponseHandler<String> packetResponseHandler =
 				new PacketResponseHandler<>(() -> cleanupCalled.set(
 				true), updateCount::set);
 
-		new Thread(() -> {
+		final Thread packetProcessor = new Thread(() -> {
 			packetResponseHandler.onPacketReceived(createPackets(1000));
 			packetResponseHandler.receivedEnd(2000);
 			packetResponseHandler.onPacketReceived(createPackets(1000));
-		}).start();
+		});
+		packetProcessor.start();
 
 		final List<String> results = packetResponseHandler.getPacketStream(30, TimeUnit.SECONDS)
 		                                                  .collect(Collectors.toList());
+
+		packetProcessor.join();
 
 		Assertions.assertTrue(cleanupCalled.get());
 		Assertions.assertEquals(2000, updateCount.get());
@@ -58,24 +66,28 @@ class PacketResponseHandlerTest {
 	}
 
 	@Test
-	void givenInterruptMidPacketStream_whenStreamPackets_thenStreamInterruptedSuccessfully() {
+	void givenInterruptMidPacketStream_whenStreamPackets_thenStreamInterruptedSuccessfully()
+			throws InterruptedException {
 		final AtomicBoolean cleanupCalled = new AtomicBoolean(false);
 		final AtomicInteger updateCount = new AtomicInteger(0);
 		final PacketResponseHandler<String> packetResponseHandler =
 				new PacketResponseHandler<>(() -> cleanupCalled.set(
 				true), updateCount::set);
 
-		new Thread(() -> {
+		final Thread packetProcessor = new Thread(() -> {
 			packetResponseHandler.onPacketReceived(createPackets(1000));
 			packetResponseHandler.receivedEnd(2000);
 			packetResponseHandler.onPacketReceived(createPackets(300));
 			packetResponseHandler.interrupt();
 			packetResponseHandler.onPacketReceived(createPackets(100));
-		}).start();
+		});
+		packetProcessor.start();
 
 		// This will work properly if it doesn't time out waiting for packets. It should exit on interrupt.
 		final List<String> results = packetResponseHandler.getPacketStream(30, TimeUnit.SECONDS)
 		                                                  .collect(Collectors.toList());
+
+		packetProcessor.join();
 
 		// clean up called on interrupt, but not on timeout
 		Assertions.assertTrue(cleanupCalled.get());
@@ -85,21 +97,24 @@ class PacketResponseHandlerTest {
 	}
 
 	@Test
-	void givenNoPacketEndReceived_whenStreamPackets_thenStreamTimesOutSuccessfully() {
+	void givenNoPacketEndReceived_whenStreamPackets_thenStreamTimesOutSuccessfully() throws InterruptedException {
 		final AtomicBoolean cleanupCalled = new AtomicBoolean(false);
 		final AtomicInteger updateCount = new AtomicInteger(0);
 		final PacketResponseHandler<String> packetResponseHandler =
 				new PacketResponseHandler<>(() -> cleanupCalled.set(
 				true), updateCount::set);
 
-		new Thread(() -> {
+		final Thread packetProcessor = new Thread(() -> {
 			packetResponseHandler.onPacketReceived(createPackets(1000));
-		}).start();
+		});
+		packetProcessor.start();
 
 		final long start = System.currentTimeMillis();
 
 		final List<String> results = packetResponseHandler.getPacketStream(50, TimeUnit.MILLISECONDS)
 		                                                  .collect(Collectors.toList());
+
+		packetProcessor.join();
 
 		// Say if this takes more than a second, it didn't time out properly. This is technically prone to a rare
 		// race condition if for some reason the thread doesn't run right away after timing out.
