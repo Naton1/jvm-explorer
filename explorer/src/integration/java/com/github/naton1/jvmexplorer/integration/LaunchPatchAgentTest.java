1c458b7d4407c7869cc4712e0103c4488b008e8b
Naton1 <28943608+Naton1@users.noreply.github.com>
2022-08-05 20:03:44 -0700

    Add more workflow logging

--- explorer/src/integration/java/com/github/naton1/jvmexplorer/integration/LaunchPatchAgentTest.java
+++ explorer/src/integration/java/com/github/naton1/jvmexplorer/integration/LaunchPatchAgentTest.java
@@ -6,6 +6,7 @@ import com.github.naton1.jvmexplorer.agent.RunningJvmLoader;
 import com.github.naton1.jvmexplorer.integration.helper.TestHelper;
 import com.github.naton1.jvmexplorer.integration.helper.TestJvm;
 import com.github.naton1.jvmexplorer.integration.programs.LaunchAnotherJvmProgram;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +14,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Properties;
 
+@Slf4j
 class LaunchPatchAgentTest {
 
 	// This is a sanity test to ensure we normally can't attach
@@ -32,7 +34,9 @@ class LaunchPatchAgentTest {
 	private long getLaunchedProcessId(TestJvm testJvm) throws Exception {
 		try (final BufferedReader br = new BufferedReader(new InputStreamReader(testJvm.getProcess().getInputStream()))
 		) {
-			return br.lines().filter(s -> s.matches("\\d+")).findFirst().map(Long::parseLong).orElseThrow();
+			return br.lines()
+					.peek(line -> log.debug("Output from launched process: {}", line))
+			         .filter(s -> s.matches("\\d+")).findFirst().map(Long::parseLong).orElseThrow();
 		}
 	}
 
